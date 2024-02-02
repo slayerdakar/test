e37dc1343c378a93ab50b25fdc20b2f3ed6d53fa
Anatolii Ivaniuk <125690572+slayerdakar@users.noreply.github.com>
2024-02-02 11:26:08 +0200

    Add files via upload

--- car/js/script.js
+++ car/js/script.js
@@ -1,39 +1,19 @@
-// const icon = document.querySelector('.icon-menu');
-// icon.addEventListener('click', function () {
-//   document.documentElement.classList.toggle('menu-open');
-// });
 
-// document.addEventListener('change', cheked)
+document.addEventListener('click', function (event) {
+  const target = event.target
 
-// function cheked(e) {
-//   const chk = e.target
-//   if (chk.tagName === 'input' && chk.type === 'checkbox') {
-//     console.log('1')
-//   }
-
-// }
-
-
-document.addEventListener('click', function () {
-  var chk = event.target
-
-  if (chk.tagName === 'INPUT' && chk.type === 'checkbox') {
-    const valSwitch = parseInt(chk.getAttribute('data-switch'))
+  if (target.type === 'checkbox') {
+    const valSwitch = parseInt(target.getAttribute('data-switch'))
     const valElem = document.querySelectorAll("[data-elem]")
-    if (chk.checked) {
-      valElem.forEach(elem => {
-        if (parseInt(elem.getAttribute('data-elem')) === valSwitch) {
-          elem.classList.add('active')
-        }
-      });
-    }
-    else {
-      valElem.forEach(elem => {
-        if (parseInt(elem.getAttribute('data-elem')) === valSwitch) {
-          elem.classList.remove('active')
-        }
-      });
-    }
+
+    if (!valElem.length) return
+
+    valElem.forEach(elem => {
+      if (parseInt(elem.getAttribute('data-elem')) === valSwitch) {
+        if (target.checked) elem.classList.add('active')
+        else elem.classList.remove('active')
+      }
+    });
   }
 })
 
