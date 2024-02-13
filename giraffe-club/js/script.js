075a2057cfdf73c00cc3c3247008305a7365426f
Anatolii Ivaniuk <125690572+slayerdakar@users.noreply.github.com>
2024-02-13 16:56:51 +0200

    Add files via upload

--- giraffe-club/js/script.js
+++ giraffe-club/js/script.js
@@ -1,2 +1,12 @@
 // Строгий режим
 "use strict"
+
+document.addEventListener("click", documentActions);
+
+function documentActions(e) {
+  const targetElement = e.target;
+
+  if (targetElement.closest('.icon-menu')) {
+    document.body.classList.toggle('menu-open');
+  }
+}
